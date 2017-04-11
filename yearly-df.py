a787e39cc78e039bf719319411f2854e29875ade
Ricky Kwok <rickyk9487@gmail.com>
2017-04-10 22:47:31 -0700

    add print statements

--- yearly-df.py
+++ yearly-df.py
@@ -2,6 +2,11 @@ from Formatting import make_one_year_team_cols_df
 
 nba_range = range(2001, 2017)
 df_by_year_off, df_by_year_def = {}, {}
+
 for year in nba_range:
     df_by_year_off[year] = make_one_year_team_cols_df(year, table_indicator="Team Stats Table")
     df_by_year_def[year] = make_one_year_team_cols_df(year, table_indicator="Opponent Stats Table")
+    
+for year in nba_range:
+    print df_by_year_off[year].head()
+    print df_by_year_def[year].head()
